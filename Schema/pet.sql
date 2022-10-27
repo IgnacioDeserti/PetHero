75cc2e40f89ac33d6aedac47e224d5bcbc9a8690
Mateo braganza <88721209+mateobraganza1@users.noreply.github.com>
2022-10-27 10:02:24 -0300

    bdd

---
+++ Schema/pet.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE IF NOT EXISTS pethero;
+
+USE pethero;
+
+CREATE TABLE IF NOT EXISTS pet(
+
+
+
+
+)Engine=InnoBD;
+
+CREATE TABLE IF NOT EXISTS guardian(
+
+
+
+
+)Engine=InnoBD;
+
+
+CREATE TABLE IF NOT EXISTS owner(
+
+
+
+
+)Engine=InnoBD;
+
+CREATE TABLE IF NOT EXISTS reserve(
+
+
+
+
+)Engine=InnoBD;
+
+CREATE TABLE IF NOT EXISTS review(
+
+
+
+
+)Engine=InnoBD;
+

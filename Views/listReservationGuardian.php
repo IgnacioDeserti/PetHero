95300ca6dd6030f84a6e47404b045c87d2d9ecdf
IgnacioMuzio <ignaciomuzio1115@gmail.com>
2022-11-17 18:18:40 -0300

    lol

--- Views/listReservationGuardian.php
+++ Views/listReservationGuardian.php
@@ -39,6 +39,33 @@
                 <div class="divListGuardian">
                     <table class="tableListGuardian">
                         <thead>
+                            <caption class="captionListGuardian"> Reservas confirmadas</th>
+                            <th class="thListGuardian">Nombre de la mascota</th>
+                            <th class="thListGuardian">Nombre del Guardian</th>
+                            <th class="thListGuardian">Fecha de inicio</th>
+                            <th class="thListGuardian">Fecha de finalizacion</th>
+                            <th class="thListGuardian">Precio</th>
+                        </thead>
+                        <tbody>
+                            <?php foreach($cReservationList as $reservation){ ?>
+                                <tr>
+                                    <td class="thListGuardian"><?php echo $allpets->getNameByIdPet($reservation->getIdPet()) ?> </td> 
+                                    <td class="thListGuardian"><?php echo $guardian->getGuardianById($reservation->getIdGuardian())->getName() ?> </td>
+                                    <td class="thListGuardian"><?php echo $reservation->getReservationDateStart() ?> </td>
+                                    <td class="thListGuardian"><?php echo $reservation->getReservationDateEnd() ?> </td>
+                                    <td class="thListGuardian"><?php echo $reservation->getPrice()?> </td>
+                                    <form action="<?= FRONT_ROOT . 'Guardian/getCoupon'?>" method = 'POST'>
+                                        <button type= 'submit' value = '<?= $reservation->getReservation()?>'>Ver cupon de pago</button>
+                                    </form>
+                                </tr>
+                            <?php }?>
+                        </tbody>                  
+                    </table>
+                </div>
+
+                <div class="divListGuardian">
+                    <table class="tableListGuardian">
+                        <thead>
                             <caption class="captionListGuardian">Reservas finalizadas</th>
                             <th class="thListGuardian">Nombre de la mascota</th>
                             <th class="thListGuardian">Nombre del Guardian</th>
@@ -59,30 +86,6 @@
                         </tbody>
                     </table>
                     
-                </div>
-
-                <div class="divListGuardian">
-                    <table class="tableListGuardian">
-                        <thead>
-                            <caption class="captionListGuardian"> Reservas confirmadas</th>
-                            <th class="thListGuardian">Nombre de la mascota</th>
-                            <th class="thListGuardian">Nombre del Guardian</th>
-                            <th class="thListGuardian">Fecha de inicio</th>
-                            <th class="thListGuardian">Fecha de finalizacion</th>
-                            <th class="thListGuardian">Precio</th>
-                        </thead>
-                        <tbody>
-                            <?php foreach($cReservationList as $reservation){ ?>
-                                <tr>
-                                    <td class="thListGuardian"><?php echo $allpets->getNameByIdPet($reservation->getIdPet()) ?> </td> 
-                                    <td class="thListGuardian"><?php echo $guardian->getGuardianById($reservation->getIdGuardian())->getName() ?> </td>
-                                    <td class="thListGuardian"><?php echo $reservation->getReservationDateStart() ?> </td>
-                                    <td class="thListGuardian"><?php echo $reservation->getReservationDateEnd() ?> </td>
-                                    <td class="thListGuardian"><?php echo $reservation->getPrice()?> </td>
-                                </tr>
-                            <?php }?>
-                        </tbody>                  
-                    </table>
                 </div>
             </form>
         </section>
